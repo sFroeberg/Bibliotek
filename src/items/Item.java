54e98496e2fe4f407ef0e194592a6f1faba047ab
Felix Holmertz <felix.holmertz@gmail.com>
2019-05-06 14:25:29 +0200

    Fixed items

---
+++ src/items/Item.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package items;
+
+import java.time.Year;
+
+/**
+ *
+ * @author froeb
+ */
+public class Item {
+    private String barcode;
+    private String title;
+    private String location;
+    private String description;
+    private Year releaseYear;
+
+    public String getBarcode() {
+        return barcode;
+    }
+
+    public void setBarcode(String barcode) {
+        this.barcode = barcode;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Year getReleaseYear() {
+        return releaseYear;
+    }
+
+    public void setReleaseYear(Year releaseYear) {
+        this.releaseYear = releaseYear;
+    }
+}
