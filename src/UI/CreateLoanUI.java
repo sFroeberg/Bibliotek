c3da3fd8910aca749b4ebc8a9ef9873d6434ff96
Felix Holmertz <felix.holmertz@gmail.com>
2019-05-22 02:28:26 +0200

    Showing receipt

--- src/UI/CreateLoanUI.java
+++ src/UI/CreateLoanUI.java
@@ -9,6 +9,10 @@ import entities.Item;
 import entities.ItemLoan;
 import entities.ItemLoanPK;
 import entities.Loan;
+import java.text.SimpleDateFormat;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.ZoneId;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
@@ -151,6 +155,7 @@ public class CreateLoanUI extends UI {
             this.getCardLayoutMain().getEntityManager().getTransaction().commit();
         }catch (RollbackException | IllegalStateException e){
             UI.showErrorDialog("Could not save loan to database");
+            return;
         }
         
         //Set itemloans in database
@@ -163,19 +168,55 @@ public class CreateLoanUI extends UI {
                 ItemLoan itemLoan = new ItemLoan();
                 ItemLoanPK itemLoanPK = new ItemLoanPK(current.getBarcode(),loan.getLoanId());
                 itemLoan.setItemLoanPK(itemLoanPK);
+                itemLoan.setItem(current);
+                itemLoan.setLoan(loan);
+                this.getCardLayoutMain().getEntityManager().persist(itemLoan);
                 itemLoans.add(itemLoan);
             }
         }
         
         try{
             //Save to database
+            //ItemLoans
             this.getCardLayoutMain().getEntityManager().getTransaction().begin();
             loan.setItemLoanCollection(itemLoans);
             this.getCardLayoutMain().getEntityManager().getTransaction().commit();
-            UI.showInfoDialog("Loan saved!");
         }catch (RollbackException | IllegalStateException e){
             UI.showErrorDialog("Could not save loan to database");
+            return;
         }
+        //Print receipt
+        String receipt = "";
+            
+        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+        Date newDate = new Date();
+
+        receipt += "Name: "+ this.getCardLayoutMain().getPatronLoggedIn().getFullName()+"\n";
+        receipt += "Loan date: "+ format.format(newDate)+"\n\n";
+        for(ItemLoan current : itemLoans){
+            System.out.println(current);
+            receipt += current.getItem().getTitle()+"\n";
+            
+            //Check if book or dvd and calculate return date as appropriate
+            if(current.getItem().getBook() == null){
+                LocalDate returnDate = LocalDate.now().plusDays(7);
+                Date finalDate = Date.from(returnDate.atStartOfDay(ZoneId.systemDefault()).toInstant());
+                
+                receipt += "To be returned: "+format.format(finalDate)+" (7 days)";
+            }else{
+                int maxLoanDays = current.getItem().getBook().getBookTypeId().getLoanDays();
+                LocalDate returnDate = LocalDate.now().plusDays(maxLoanDays);
+                Date finalDate = Date.from(returnDate.atStartOfDay(ZoneId.systemDefault()).toInstant());
+                
+                receipt += "To be returned: "+format.format(finalDate)+" ("+maxLoanDays+" days)";
+            }
+            receipt += "\n\n";
+        }
+        //Remove last line breaks
+        receipt = receipt.trim();
+
+        UI.showInfoDialog("Loan saved!\n\nThis is your receipt:\n"+receipt);
+        
     }//GEN-LAST:event_createLoanBtnActionPerformed
 
     private void addItemBtnActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_addItemBtnActionPerformed
