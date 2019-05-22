d4770083194dd931fcabd6f06e3f9a6c61542850
Sebastian <froebergis@gmail.com>
2019-05-22 13:26:26 +0200

    stashingForPull

--- src/entities/Item.java
+++ src/entities/Item.java
@@ -180,7 +180,7 @@ public class Item implements Serializable {
 
     @Override
     public String toString() {
-        return "entities.Item[ barcode=" + barcode + " ]";
+        return title;
     }
     
 }
